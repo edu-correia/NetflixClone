f1a9cd691aa0fa10e63fef074cb3524bf2ef8651
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2022-04-11 18:44:03 -0300

    Main activity rendering multiples carousels

--- app/src/main/java/com/educorreia/netflixclone/MainActivity.java
+++ app/src/main/java/com/educorreia/netflixclone/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.educorreia.netflixclone.models.Category;
 import com.educorreia.netflixclone.models.Movie;
 
 import org.w3c.dom.Text;
@@ -25,19 +26,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView rcvMoviesList = findViewById(R.id.rcv_movies_list);
+        RecyclerView rcvCategoriesList = findViewById(R.id.rcv_categories_list);
 
-        List<Movie> movies = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-            Movie movie = new Movie();
-            movie.setCoverUrl(R.drawable.movie_cover);
-            movies.add(movie);
+        List<Category> categories = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Category category = new Category();
+
+            List<Movie> movies = new ArrayList<>();
+            for (int j = 0; j < 30; j++) {
+                Movie movie = new Movie();
+                movie.setCoverUrl(R.drawable.movie_cover);
+                movies.add(movie);
+            }
+
+            category.setName("Categoria: " + i);
+            category.setMovies(movies);
+
+            categories.add(category);
         }
 
-        MoviesListAdapter adapter = new MoviesListAdapter(movies);
+        CategoriesListAdapter adapter = new CategoriesListAdapter(categories);
+        rcvCategoriesList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rcvCategoriesList.setAdapter(adapter);
+    }
 
-        rcvMoviesList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        rcvMoviesList.setAdapter(adapter);
+    private static class CategoryHolder extends RecyclerView.ViewHolder {
+        TextView txtCategoryName;
+        RecyclerView rcvMoviesList;
+
+        public CategoryHolder(@NonNull View itemView) {
+            super(itemView);
+            txtCategoryName = itemView.findViewById(R.id.txt_category_name);
+            rcvMoviesList = itemView.findViewById(R.id.rcv_movies_list);
+        }
+    }
+
+    private class CategoriesListAdapter extends RecyclerView.Adapter<CategoryHolder>{
+        private final List<Category> categoriesList;
+
+        public CategoriesListAdapter(List<Category> categoriesList) {
+            this.categoriesList = categoriesList;
+        }
+
+        @NonNull
+        @Override
+        public CategoryHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new CategoryHolder(getLayoutInflater().inflate(R.layout.category_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CategoryHolder holder, int position) {
+            Category category = categoriesList.get(position);
+            holder.txtCategoryName.setText(category.getName());
+
+            MoviesListAdapter adapter = new MoviesListAdapter(category.getMovies());
+            holder.rcvMoviesList.setLayoutManager(new LinearLayoutManager(getBaseContext(), LinearLayoutManager.HORIZONTAL, false));
+            holder.rcvMoviesList.setAdapter(adapter);
+        }
+
+        @Override
+        public int getItemCount() {
+            return categoriesList.size();
+        }
     }
 
     private static class MovieHolder extends RecyclerView.ViewHolder {
